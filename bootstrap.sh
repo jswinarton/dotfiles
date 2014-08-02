c4032fc7f2c724e23a721d6d3e0d24d7b7a5e0d0
Jeremy Swinarton <jeremy@swinarton.com>
2014-08-02 12:21:34 -0400

    auto add gitignore global

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,10 @@ git pull origin master
 function do_it() {
     ~/.osx
     ln -Ffs ~/.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+    ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude ".extra" \
         --exclude "README" --exclude "LICENSE" -av --no-perms . ~
-    ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+    git config --global core.excludesfile '~/.gitignore_global'
     source ~/.profile
 }
 
