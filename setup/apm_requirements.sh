ed38afdc600cb1e50a794d0a5be912455cd04e95
Jeremy Swinarton <jeremy@swinarton.com>
2015-04-24 15:12:38 -0400

    use shell scripts instead

---
+++ setup/apm_requirements.sh
@@ -0,0 +1,6 @@
+apm install autocomplete-plus
+apm install linter
+apm install linter-flake8
+apm install linter-jshint
+apm install monokai
+apm install pain-split
---
+++ setup/brew_requirements.sh
@@ -0,0 +1,11 @@
+brew install git-flow
+brew install heroku-toolbelt
+brew install ffmpeg --with-theora --with-lib-vpx
+brew install nginx
+brew install nmap
+brew install node
+brew install postgresql
+brew install python
+brew install python3
+brew install rbenv
+brew install zsh
