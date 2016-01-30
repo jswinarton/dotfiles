fc83a73ee3363aa4f0f844229b643f42a27f4819
Jeremy Swinarton <jeremy@swinarton.com>
2016-01-30 14:59:50 -0500

    reorg and add vundle install to setup

--- setup/setup_common.sh
+++ setup/setup_common.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=$(cd $CURRENT_DIR && cd ../ && pwd)
+DOTFILE_DIR=$ROOT_DIR/dotfiles
+
+
 ###
 # Symlink dotfiles
 
 shopt -s dotglob
 
-DOTFILE_PATH_RELATIVE="../dotfiles"
-DOTFILE_PATH="$(cd $DOTFILE_PATH_RELATIVE && pwd)";
-
-for f in $DOTFILE_PATH/*
+for f in $DOTFILE_DIR/*
 do
     DOTFILE_NAME="$(basename $f)"
-    SRC_FILE=$DOTFILE_PATH/$DOTFILE_NAME
+    SRC_FILE=$DOTFILE_DIR/$DOTFILE_NAME
     DST_FILE=$HOME/$DOTFILE_NAME
 
-    ln -sf $SRC_FILE $DST_FILE
+    ln -s $SRC_FILE $DST_FILE
 done
+
+
+###
+# Vim plugins
+
+git clone https://github.com/VundleVim/Vundle.vim.git $DOTFILE_DIR/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
