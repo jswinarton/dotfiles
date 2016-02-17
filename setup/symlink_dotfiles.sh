e1ca0fd2619dec8a50e3045afd51ed7fc49e5976
Jeremy Swinarton <jeremy@swinarton.com>
2016-02-17 17:32:40 -0500

    Update setup script

--- setup/symlink_dotfiles.sh
+++ setup/symlink_dotfiles.sh
@@ -4,9 +4,6 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$(cd $CURRENT_DIR && cd ../ && pwd)
 DOTFILE_DIR=$ROOT_DIR/dotfiles
 
-
-###
-# Symlink dotfiles
 shopt -s dotglob
 
 for f in $DOTFILE_DIR/*
@@ -17,14 +14,3 @@ do
 
     ln -sf $SRC_FILE $DST_FILE
 done
-
-
-###
-# Vim plugins
-git clone https://github.com/VundleVim/Vundle.vim.git $DOTFILE_DIR/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-
-
-###
-# Shell upgrades
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
